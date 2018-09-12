04d94863919d596acbea16143a5894a875e657e6
SonyEvent1 <43119557+SonyEvent1@users.noreply.github.com>
2018-09-12 23:55:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,201 @@ client.on('ready', () => {
   console.log('')
 });
 
+client.on('message', message => {
+      if (message.author.bot) return;
+      var prefix ="="
+       if (message.content === prefix + "help-admin") {
+        if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+       message.channel.send('**تم ارسال رسالة في الخاص**');
+  
+  
+  
+  
+   message.author.sendMessage(`
+   **
+  [❖═════اومر ادمن═══════❖]
+  ❖ =kick <mention > ➾  kickلي اعطاء شخص
+
+  ❖ =setvoice  لي انشاء روم فويس اونلاين
+  
+  ❖ =say  ➾ يكرر الكلام الذي تقولة
+  
+  ❖ =ban <mention> ➾ لي اعطاء شخص بان
+  
+  ❖ =unban <mention> ➾ لي فك بان عن شخص
+  
+  ❖ =clear ➾ لي مسح الشات
+  
+  ❖ =mute <mention> ➾ لي اعطاء ميوت لي شخص
+  
+  ❖ =ct <name> ➾ لي انشاء روم كتبي
+  
+  ❖ =cv <name> ➾لي انشاء روم صوتي
+  
+  ❖ =bc <message>  ➾ لي ارسال رسالة لي كل الاعضاء
+  **
+  `);
+  
+      }
+  });
+  
+  
+
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+client.on("ready", async () => {
+    if(client.guilds.size > 1) {
+        client.user.setActivity(`Im on ${client.guilds.size} servers!`);
+        console.log(`${client.user.username} is online on ${client.guilds.size} servers!`)
+    } else {
+        client.user.setActivity(`Im on ${client.guilds.size} servers!`);
+        console.log(`${client.user.username} is online on ${client.guilds.size} server!`)
+    }
+    client.user.setStatus("online");
+});
+
+client.on("guildCreate", guild => {
+    console.log("Joined a new guild: " + guild.name);
+    if(client.guilds.size > 1) {
+        client.user.setActivity(`Im on ${client.guilds.size} servers!`);
+    } else {
+        client.user.setActivity(`Im on ${client.guilds.size} servers!`);
+    }
+});
+
+
+
+
+
+
+
+client.on('message', message =>{
+    if(message.content === '=ping'){
+let start = Date.now(); message.channel.send('pong').then(message => { 
+message.edit(`\`\`\`js
+Time taken: ${Date.now() - start} ms
+Discord API: ${client.ping.toFixed(0)} ms\`\`\``);
+    });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message=>{
+var prefix = "=" // البريفكس حقك هنا 
+
+
+    let nour;
+    if (message.mentions.users.first()) {
+      nour = message.mentions.users.first();
+    } else {
+        nour = message.author;
+    }
+
+  var roles;
+      if (message.member.roles.size === 1) {
+          roles = '`None | لا يوجد`';
+      } else {
+          roles = message.member.roles.map(roles => `\`${roles.name}\``).join(', ')
+      }
+
+  if(message.content === prefix + 'myroles'){
+    if(message.author.bot) return;
+let myroles = new Discord.RichEmbed()
+.setColor('#4CE782')
+.setAuthor(nour.tag, nour.avatarURL)
+.setThumbnail(nour.avatarURL)
+.setTitle('MyRoles | رتبي :')
+.setDescription(`**# - Roles | الرتب **: ${roles}`)
+.setFooter(message.author.username, message.author.avatarURL)
+.setTimestamp() 
+message.channel.send(myroles)
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("guildDelete", guild => {
+    console.log("Left a guild: " + guild.name);
+    if(client.guilds.size > 1) {
+        client.user.setActivity(`Im on ${client.guilds.size} servers!`);
+    } else {
+        client.user.setActivity(`Im on ${client.guilds.size} servers!`);
+    }
+});
+
+client.on("message", async () => {
+  
+})
+
+  
+
+  
+  
+  
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('السبة')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تسب",
+        color: 0x06DF00,
+        description: "مَّا يَلْفِظُ مِن قَوْلٍ إِلَّا لَدَيْهِ رَقِيبٌ عَتِيدٌ ",
+        footer: {
+          text: "By Abo Khalil"
+        }
+      }}).then(msg => {msg.delete(4000)});
+                          }
+
+
+});
 
 
 client.login(process.env.BOT_TOKEN);
