6e89e549920907a2bdb50fcde50206e8d7d94b2b
SonyEvent1 <43119557+SonyEvent1@users.noreply.github.com>
2018-09-13 16:29:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -212,23 +212,7 @@ message.channel.send(myroles)
 
 
 
-client.on('ready', function(){
-    var ms = 10000 ;
-    var setGame = [' Ganger ','For | Ever','Perfix ','=','Enjoy!'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`https://www.twitch.tv/n3k4a`);
-    }, ms);
 
-})
   
 
   
